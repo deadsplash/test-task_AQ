4d4ab8872e4d4f1cff2855453eb50cd95f032509
deadsplash <deadsplashmail@gmail.com>
2021-10-18 15:07:48 +0300

    prettified code

--- main.py
+++ main.py
@@ -1,12 +1,19 @@
-from flask import Flask, render_template, redirect
+from flask import Flask, redirect
 import pandas as pd
 import pygsheets
 
 app = Flask(__name__)
-
 gc = pygsheets.authorize()
-sh = gc.open_by_url('https://docs.google.com/spreadsheets/d/1A3xQOY8P0juEfEf_xMXkMs-zXPu3R6FPdCylmM7nSJI/edit#gid=0')
-wks = sh.sheet1
+table_url = 'https://docs.google.com/spreadsheets/d/1A3xQOY8P0juEfEf_xMXkMs-zXPu3R6FPdCylmM7nSJI/edit#gid=0'
+
+
+def grabber(url):
+    sh = gc.open_by_url(url)
+    main_sheet = sh.sheet1
+    return main_sheet
+
+
+wks = grabber(table_url)
 
 df = wks.get_as_df()
 
@@ -24,4 +31,3 @@ def index():
 
 if __name__ == "__main__":
     app.run(debug=True, port=3000)
-
